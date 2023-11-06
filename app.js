ac8d176db1fa1dae5a9bdb2056835ddb9733028d
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-11-06 15:33:58 -0300

    Comenzando proyecto

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express') 
+const cors  = require('cors') // le decimos a la app que use cors, esta es una libreria que me permite hacer peticiones remotas
+const app = express()
+
+app.use(cors())
+
+const port = 3000
+
+app.listen(port,() => {
+    console.log(`Server running on port ${port}`)
+})
