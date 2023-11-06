e84edbe7731663d29e7a9ee3419572f3e9c359d2
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-11-06 16:19:18 -0300

    Scaffolding

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+require("dotenv").config()
+
 const express = require('express') 
 const cors  = require('cors') // le decimos a la app que use cors, esta es una libreria que me permite hacer peticiones remotas
 const app = express()
 
 app.use(cors())
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port,() => {
     console.log(`Server running on port ${port}`)
