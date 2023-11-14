a3703cf7c297ccb4d0da87a3ece664212860d704
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-11-14 20:55:52 -0300

    Documentacion Tracks

--- routes/tracks.js
+++ routes/tracks.js
@@ -10,15 +10,94 @@ const checkRol = require('../utils/Middlewares/roles_middleware');
 
 //Rutas TRACKS
 
+
+/**
+ * Get all tracks
+ * @openapi
+ * /tracks:
+ *    get:
+ *      tags:
+ *        - tracks
+ *      summary: "Listar canciones"
+ *      description: Obten todas las listas de las canciones
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *        '200':
+ *          description: Retorna la listas de las canciones.
+ *          content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/track'
+ *        '422':
+ *          description: Error de validacion.
+ */
 router.get('/', 
 authMiddleware, 
 controller_tracks.getTracksNames);
 
+/**
+ * Get track
+ * @openapi
+ * /tracks/{id}:
+ *    get:
+ *      tags:
+ *        - tracks
+ *      summary: "Detalle cancion"
+ *      description: Obten el detalle de una cancion
+ *      security:
+ *        - bearerAuth: []
+ *      parameters:
+ *      - name: id
+ *        in: path
+ *        description: ID de canción a retornar
+ *        required: true
+ *        schema:
+ *          type: string
+ *      responses:
+ *        '200':
+ *          description: Retorna el objecto de la cancion.
+ *          content:
+ *             application/json:
+ *               schema:
+ *                   $ref: '#/components/schemas/track'
+ *        '422':
+ *          description: Error de validacion.
+ */
 router.get('/:id', 
 authMiddleware,
 validatorId, 
 controller_tracks.getTrack);
 
+/**
+ * Register new track
+ * @openapi
+ * /tracks:
+ *    post:
+ *      tags:
+ *        - tracks
+ *      summary: "Register track"
+ *      description: Registra una cancion y obtener el detalle del registro
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *        '200':
+ *          description: Retorna el objeto insertado en la coleccion.
+ *        '422':
+ *          description: Error de validacion.
+ *      requestBody:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                 $ref: "#/components/schemas/track"
+ *    responses:
+ *      '201':
+ *        description: Retorna el objeto insertado en la coleccion con stado '201'
+ *      '403':
+ *        description: No tiene permisos '403'
+ */
 router.post(
   '/',
   authMiddleware,
@@ -27,6 +106,44 @@ router.post(
   controller_tracks.createTrack
 );
 
+/**
+ * Update track
+ * @openapi
+ * /tracks/{id}:
+ *    put:
+ *      tags:
+ *        - tracks
+ *      summary: "Update track"
+ *      description: Actualiza una cancion y obtener el detalle del registro
+ *      security:
+ *        - bearerAuth: []
+ *      parameters:
+ *      - name: id
+ *        in: path
+ *        description: ID de canción a retornar
+ *        required: true
+ *        schema:
+ *          type: string
+ *      responses:
+ *        '200':
+ *          description: Retorna el objeto actualizado en la coleccion.
+ *        '422':
+ *          description: Error de validacion.
+ *      requestBody:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                 $ref: "#/components/schemas/track"
+ *    responses:
+ *      '201':
+ *        description: Retorna el objeto insertado en la coleccion con stado '201'
+ *        content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/track'
+ *      '403':
+ *        description: No tiene permisos '403'
+ */
 router.put(
   '/:id',
   authMiddleware,
@@ -35,6 +152,30 @@ router.put(
   controller_tracks.updateTrack
 );
 
+/**
+ * Delete track
+ * @openapi
+ * /tracks/{id}:
+ *    delete:
+ *      tags:
+ *        - tracks
+ *      summary: "Eliminar cancion"
+ *      description: Elimiar el detalle de una cancion
+ *      security:
+ *        - bearerAuth: []
+ *      parameters:
+ *      - name: id
+ *        in: path
+ *        description: ID de canción a retornar
+ *        required: true
+ *        schema:
+ *          type: string
+ *      responses:
+ *        '200':
+ *          description: Retorna el objecto de la cancion.
+ *        '422':
+ *          description: Error de validacion.
+ */
 router.delete(
   '/:id',
   authMiddleware,
